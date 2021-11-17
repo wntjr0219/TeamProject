5b647a913e5b103d7be31adc09280f9a0dac4a48
wntjr0219 <90208937+wntjr0219@users.noreply.github.com>
2021-11-17 18:01:05 +0900

    몬스터, 맵 오브젝트생성

--- monster_object.py
+++ monster_object.py
@@ -1,75 +1,49 @@
 from pico2d import *
 import random
-#3주차 적군 기본 오브젝트
+import game_framework
+#8(?)주차 적군 기본 오브젝트
 
 #아직 샘플만 구현됨
 
-class Grass:
-    def __init__(self): 
-        self.image = load_image('grass.png')
+PIXEL_PER_METER = (10.0 / 0.3) # 0.3미터당 10픽셀
+RUN_SPEED_KMPH = 5.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS* PIXEL_PER_METER)
 
-    def draw(self):
-        self.image.draw(400, 30)
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
 
 class Monster:
+    image = None
+
     def __init__(self): 
-        self.image = load_image('gomba_motion.png')
         self.x, self.y = random.randint(100, 700), random.randint(70, 80)
         self.frame = 0
         self.act = 0
+        if Monster.image == None:
+            self.image = load_image('gomba_motion.png')
+
     def update(self): 
-        self.frame = (self.frame + 1) % 7
+        global RUN_SPEED_PPS
+        self.frame = (self.frame +  FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
+        
+        timer = random.randint(200, 400)
+        dir = random.randint(-1, 1)
+        self.act = dir
+        # 움직임 대충 구현
+        while timer > 0:
+            velocity = RUN_SPEED_PPS * dir
+            self.x += velocity * game_framework.frame_time
+            timer -= 1
+            delay(0.05)
 
-        next_x = self.x + random.randint(-20, 20)
-        if self.x > next_x:
-                self.act = 1
-        else:
-            self.act = 0
-
-        if next_x <= 800 and next_x >= 0:
-            self.x = next_x
-
-        if next_x > 800:
-            self.x = 800
-        if next_x < 0:
-            self.x = 0
+        self.x = clamp(25, self.x, 800 - 25)
         
         
         
     def draw(self): 
-        self.image.clip_draw(self.frame*65, self.act * 75, 65, 75, self.x, self.y)
-
-def handle_events():
-    global running
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        self.image.clip_draw(int(self.frame) * 65, self.act * 75, 65, 75, self.x, self.y)
 
 
-open_canvas()
-
-grass = Grass()
-
-running = True
-# goombas = [Monster() for i in range(20)] #
-goomba = Monster()
-while running:
-    
-
-    handle_events()
-
-    # for goomba in goombas:
-    goomba.update()
-
-    clear_canvas()
-    grass.draw()
-
-    # for goomba in goombas:
-    goomba.draw()
-
-    update_canvas()
-
-    delay(0.1)
